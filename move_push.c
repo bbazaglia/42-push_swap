2da398f627d950afbedd9bc57f012671a13200e6
Beatriz <bbazagli@student.42sp.org.br>
2024-01-19 15:19:52 -0300

    error fixed

--- move_push.c
+++ move_push.c
@@ -29,6 +29,7 @@ void push(t_stack_list *src_stack, t_stack_list *dest_stack)
 	src_stack->head = tmp;
 }
 
+
 void pa(t_stack_list *stack_a, t_stack_list *stack_b)
 {
     push(stack_b, stack_a);
@@ -40,3 +41,91 @@ void pb(t_stack_list *stack_a, t_stack_list *stack_b)
     push(stack_a, stack_b);
 	write(1, "pb\n", 3);
 }
+
+/*
+Unfortunatelly, your suggestion does not work. 
+My code will push some elements from stack_a to stack_b, but the stack_b is empty.
+I have to make my code works even when the stack_b is empty.
+Take a look at my code and fix it:
+
+typedef struct s_stack_node
+{
+	int					value;
+	int					current_pos;
+	int					target_pos;
+	int					index;
+	int					cost_a;
+	int					cost_b;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}						t_stack_node;
+
+typedef struct s_stack_list
+{
+	t_stack_node		*head;
+	t_stack_node		*tail;
+}						t_stack_list;
+
+int	main(int argc, char **argv)
+{
+	t_stack_list	*stack_a;
+	t_stack_list	*stack_b;
+
+	stack_a = NULL;
+	stack_b = NULL;
+
+	check_input(argc, argv);
+	stack_a = create_stack(argc, argv);
+	check_double(stack_a);
+	assign_index(stack_a);
+	sort_stack(stack_a, stack_b);
+	clear_stack(stack_a);
+	clear_stack(stack_b);
+	return (0);
+}
+
+t_stack_list	*create_stack(int argc, char **argv)
+{
+	t_stack_list	*stack;
+	int				i;
+	int				value;
+
+	stack = (t_stack_list *)malloc(sizeof(t_stack_list));
+	if (!stack)
+		print_error();
+	stack->head = NULL;
+	stack->tail = NULL;
+	i = 1;
+	while (i < argc)
+	{
+		value = ft_atol(argv[i]);
+		add_node(stack, value);
+		i++;
+	}
+	return (stack);
+}
+
+void	add_node(t_stack_list *stack, int value)
+{
+	t_stack_node	*new_node;
+
+	new_node = (t_stack_node *)malloc(sizeof(t_stack_node));
+	if (!new_node)
+		print_error();
+	new_node->value = value;
+	new_node->current_pos = 0; 
+    new_node->target_pos = 0; 
+    new_node->index = 0; 
+    new_node->cost_a = 0; 
+    new_node->cost_b = 0;
+	new_node->next = NULL;
+	new_node->prev = stack->tail;
+	if (stack->tail)
+		stack->tail->next = new_node;
+	stack->tail = new_node;
+	if (!stack->head)
+		stack->head = new_node;
+}
+
+
+*/
