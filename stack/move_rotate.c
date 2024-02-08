a535fc0a915aea01ab7d4803fc5b2272c56c8260
Beatriz <beatriz@bazaglia.com>
2024-02-08 11:57:14 -0300

    removed comment

--- stack/move_rotate.c
+++ stack/move_rotate.c
@@ -28,41 +28,6 @@ void	rotate(t_stack_list *stack)
 	stack->tail = tmp;
 }
 
-// the first element becomes the last one
-// void	rotate(t_stack_list *stack)
-// {
-// 	t_stack_node	*tmp;
-
-// 	check if the stack is empty or contains only one node.
-// 	if (stack->head == NULL || stack->head->next == NULL)
-// 		return ;
-// 	tmp stores the first node
-// 	tmp = stack->head;
-// 	if (stack->head->next->index == stack->tail->index)
-// 	{
-// 		stack->head = stack->head->next;
-// 		stack->head->prev = NULL;
-// 		stack->head->next = tmp;
-// 		stack->tail = tmp;
-// 		stack->tail->prev = stack->head;
-// 		stack->tail->next = NULL;
-// 	}
-// 	else
-// 	{
-// 		the second node becomes the new first
-// 		stack->head = stack->head->next;
-// 		break the connection between the first node and the rest of the stack
-// 		stack->head->prev = NULL;
-// 		the "old first" node becomes the last one
-// 		stack->tail->next = tmp;
-// 		tmp->prev = stack->tail;
-// 		tmp->next = NULL;
-// 		update the tail pointer
-// 		stack->tail = tmp;
-// 	}
-// }
-
-
 void	ra(t_stack_list *stack_a)
 {
 	rotate(stack_a);
